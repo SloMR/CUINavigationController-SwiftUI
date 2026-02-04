e86a32b1eb3d2e2cd436f1a8af50e49876b060ed
Sulaiman <xslomxsa@gmail.com>
2026-02-04 19:36:22 +0300

    Initial Commit

---
+++ CustomNav/CustomNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomNavApp.swift
+//  CustomNav
+//
+//  Created by Sulaiman Alromaih on 04/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct CustomNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
